ce04afc091bcdf596b0448bdf8d255ca69b01cd9
hanfc <2624366594@qq.com>
2023-11-30 11:17:23 +0800

    Rcpp

--- src/MNR.cpp
+++ src/MNR.cpp
@@ -1,6 +1,7 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
+#define ARMA_64BIT_WORD
 #define ARMA_DONT_PRINT_ERRORS
 #include "RcppArmadillo.h"
 #include "stdlib.h"
